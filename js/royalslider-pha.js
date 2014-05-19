41ab372eb4088dd9ac65882350e32ff97e80f2b9
Blair McClenny <blairmmcclenny@BLAIR-M-MCCLENNYs-MacBook-Pro.local>
2014-05-19 15:31:00 -0400

    v1.00

---
+++ js/royalslider-pha.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$('.royalSlider').royalSlider({
+		autoScaleSlider: true,
+		imageScalePadding: 0,
+		controlNavigation: 'none',
+		arrowsNavAutoHide: false,
+		loop: true,
+		controlsInside: false,
+		navigateByClick: false,
+		globalCaption: true,
+		autoPlay: {
+    		enabled: true,
+    		pauseOnHover: true
+    	}
+    });
+
+    var rs = $('.royalSlider').data('royalSlider');
+	
+	// Init slide number.
+	$('.royalSlider').append('<div class="rsSlideCount"></div>');
+	$('.rsSlideCount').text("1 of " + rs.numSlides );
+
+	// Update slide number.
+	rs.ev.on('rsAfterSlideChange', function() {
+		$('.rsSlideCount').text(rs.currSlideId + 1 + " of " + rs.numSlides);
+	});
+
+});
